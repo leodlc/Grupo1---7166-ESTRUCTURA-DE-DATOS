e331b089d5b614c49a3148a1b148ac996046262a
StevenJossueAchig <stevenjossue5@gmail.com>
2022-01-07 00:27:57 -0500

    ARBOLES AVL

--- II PARCIAL/TAREAS/Tarea 3 Arboles/Nodo.h
+++ II PARCIAL/TAREAS/Tarea 3 Arboles/Nodo.h
@@ -4,33 +4,33 @@ using namespace std;
 class Nodo
 {
 private:
-int dato;
-Nodo* izquierda;
-Nodo* derecha;
+	int dato;
+	Nodo* izquierda;
+	Nodo* derecha;
 public:
-void setDato(int);
-void setIzquierda(Nodo*);
-void setDerecha(Nodo*);
-int getDato();
-Nodo*& getIzquierda();
-Nodo*& getDerecha();
-/*devuelve la informacion del nodo*/
+	void setDato(int);
+	void setIzquierda(Nodo*);
+	void setDerecha(Nodo*);
+	int getDato();
+	Nodo*& getIzquierda();
+	Nodo*& getDerecha();
+	/*devuelve la informacion del nodo*/
 };
-void Nodo::setDato(int _dato){
-dato = _dato;
+void Nodo::setDato(int _dato) {
+	dato = _dato;
 }
-void Nodo::setIzquierda(Nodo* _izquierda){
-izquierda = _izquierda;
+void Nodo::setIzquierda(Nodo* _izquierda) {
+	izquierda = _izquierda;
 }
-void Nodo::setDerecha(Nodo* _derecha){
-derecha = _derecha;
+void Nodo::setDerecha(Nodo* _derecha) {
+	derecha = _derecha;
 }
-int Nodo::getDato(){
-return dato;
+int Nodo::getDato() {
+	return dato;
 }
-Nodo*& Nodo::getIzquierda(){
-return izquierda;
+Nodo*& Nodo::getIzquierda() {
+	return izquierda;
 }
-Nodo*& Nodo::getDerecha(){
-return derecha;
+Nodo*& Nodo::getDerecha() {
+	return derecha;
 }
